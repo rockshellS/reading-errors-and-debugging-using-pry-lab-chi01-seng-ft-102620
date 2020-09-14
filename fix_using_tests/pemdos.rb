3b3f99dd2bed7f117d19abd469ea36642e6918e7
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-14 20:03:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,8 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  if string[0] == "s".to_i 
+    10.times  string
   else
     string
   end
